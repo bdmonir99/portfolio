342d529434014f124591f62286d59aed12e337ed
MD Monir Hossain <47349248+bdmonir99@users.noreply.github.com>
2026-06-25 19:27:01 +0600

    Add files via upload

---
+++ theCompiler360/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $("#aiChatBoatBtn").click(function(){
+    $("#aiChatArea").toggleClass("showAiChat");
+  });
+  $("#closeBot").click(function(){
+    $("#aiChatArea").removeClass("showAiChat");
+  });
+});
+
+$('#demo').dcalendarpicker();
+$('#calendar-demo').dcalendar(); //creates the calendar
+
+
+
+
